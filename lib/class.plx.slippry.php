0856dce58c2417c2edc0ad00fb508314c582043a
Stephane <Stphane@pluxml.org>
2017-04-10 15:10:08 +0200

    controle existence PLX_MYMULTILINGUE + maj infos

--- lib/class.plx.slippry.php
+++ lib/class.plx.slippry.php
@@ -6,8 +6,10 @@ class slippry {
 	public $aSlides = array(); # tableau des slides
 
 	public function __construct($default_lang) {
-		$lang = plxMyMultiLingue::_Lang();
-		if(!empty($lang) AND defined('PLX_ADMIN')) $default_lang = $lang;
+		if(defined('PLX_MYMULTILINGUE')) {
+			$lang = plxMyMultiLingue::_Lang();
+			if(!empty($lang) AND defined('PLX_ADMIN')) $default_lang = $lang;
+		}
 		$this->config = PLX_ROOT.PLX_CONFIG_PATH.'plugins/slippry.config.'.$default_lang.'.xml';
 	}
 
